87e8ecb91812cafe0b8ca8307b2df326db34f4d5
DanielaDiazA <danidiar.14@gmail.com>
2025-12-02 22:07:06 -0600

    Creación de proyectos y referencias

---
+++ Practica4/Practica4.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Practica4.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Practica4/Practica4.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica4.API.WebApiApplication" Language="C#" %>
